8965b469f4f4b86defde7ca21d5f846496a44c32
Han <xsytby1112@gmail.com>
2020-02-11 11:43:25 +0800

    feat: add nuxt

--- examples/main/index.js
+++ examples/main/index.js
@@ -58,6 +58,7 @@ registerMicroApps(
     { name: 'react16-main', entry: '//localhost:7100', render, activeRule: genActiveRule('/react') },
     { name: 'react15 app', entry: '//localhost:7102', render, activeRule: genActiveRule('/15react15') },
     { name: 'vue app', entry: '//localhost:7101', render, activeRule: genActiveRule('/vue') },
+    { name: 'nuxt app', entry: '//127.0.0.1:8080', render, activeRule: genActiveRule('/nuxt') },
   ],
   {
     beforeLoad: [
